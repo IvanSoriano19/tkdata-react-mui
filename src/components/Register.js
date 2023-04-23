002d49c8668ba2697850362c68e6b336ea12dcbb
Ivan Soriano <ivansoriano1901@gmail.com>
2023-04-23 20:42:07 +0200

    auth register firebase

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,48 +1,102 @@
 import { TextField, Button, makeStyles } from "@material-ui/core";
 import { useState } from "react";
+import { useAuth } from "../context/authContext";
+import { useNavigate } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
     register: {
-        alignItems: "center",
-        width: "30%",
         display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        margin: "15% 36%",
     },
+    textFieldStyle:{
+        padding: 6
+    },
+    palette:{
+        primary: "#447CC1"
+    },
+    buttonStyle:{
+        alignItems: "center",
+        justifyContent: "center"
+    }
 }));
 
 export function Register(props) {
-    const classes = useStyles()
-
+    const classes = useStyles();
     const [user, setUser] = useState({
         name: "",
         provincia: "",
         email: "",
         telefono: "",
         password: "",
-        password2: "",
     });
+
+    const {signup} = useAuth()
+    const navigate = useNavigate()
+    const [error, setError] = useState()
+
+    const handleChange = ({ target: { id, value } }) => {
+        setUser({ ...user, [id]: value });
+    };
+
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+        try {
+            await signup(user.email, user.password)
+            navigate("/")
+        } catch (error) {
+            setError(error.message) 
+            console.log(error.message)
+            
+        }
+    };
 
     return (
         <div className={classes.register}>
-            <form>
+
+            {error && <p>{error}</p>}
+
+            <form onSubmit={handleSubmit} >
                 <TextField
                     id="name"
                     label="Nombre del club"
-                    variant="standard"
+                    variant="outlined"
+                    onChange={handleChange}
+                    className={classes.textFieldStyle}
                 />
                 <TextField
                     id="provincia"
                     label="Provincia"
-                    variant="standard"
+                    variant="outlined"
+                    onChange={handleChange}
+                    className={classes.textFieldStyle}
                 />
-                <TextField id="email" label="Email" variant="standard" />
-                <TextField id="telefono" label="Telefono" variant="standard" />
-                <TextField id="password" label="Password" variant="standard" />
                 <TextField
-                    id="password2"
-                    label="Password2"
-                    variant="standard"
+                    id="email"
+                    label="Email"
+                    type="email"
+                    variant="outlined"
+                    onChange={handleChange}
+                    className={classes.textFieldStyle}
                 />
-                <Button variant="contained">Registrar</Button>
+                <TextField
+                    id="telefono"
+                    label="Telefono"
+                    variant="outlined"
+                    onChange={handleChange}
+                    className={classes.textFieldStyle}
+                />
+                <TextField
+                    id="password"
+                    label="Password"
+                    type="password"
+                    variant="outlined"
+                    onChange={handleChange}
+                    className={classes.textFieldStyle}
+                />
+                <br></br>
+                <Button variant="contained" type="submit" className={classes.buttonStyle}>Registrar</Button>
             </form>
         </div>
     );
--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,4 +1,6 @@
 import { createContext, useContext } from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase-config";
 
 export const authContext = createContext();
 
@@ -9,11 +11,13 @@ export const useAuth = () => {
 };
 
 export function AuthProvider({ children }) {
-    const user = {
-        login: true,
+    const signup = (email, password) => {
+        createUserWithEmailAndPassword(auth, email, password)
     };
 
     return (
-        <authContext.Provider value={{ user }}>{children}</authContext.Provider>
+        <authContext.Provider value={{ signup }}>
+            {children}
+        </authContext.Provider>
     );
 }
