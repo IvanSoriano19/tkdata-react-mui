7feb399f49a91047adc623b2be70ec30e1ef75f6
Ivan Soriano <ivansoriano1901@gmail.com>
2023-05-16 19:03:06 +0200

    fix: delete usecallbak

--- src/components/pages/miClub/MiClub.js
+++ src/components/pages/miClub/MiClub.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, {  useEffect, useState } from "react";
 import { Navbar } from "../../home/Navbar";
 import {
     Container,
