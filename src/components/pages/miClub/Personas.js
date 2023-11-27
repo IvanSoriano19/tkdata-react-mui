914d99d0aeb0a12487707a9eb6b4c4a40d05e829
Ivan Soriano <ivansoriano1901@gmail.com>
2023-11-27 21:22:23 +0100

    feat: el filtrado del peso funciona

--- src/components/pages/miClub/Personas.js
+++ src/components/pages/miClub/Personas.js
@@ -10,9 +10,12 @@ import {
     MenuItem,
     Select,
     InputLabel,
-    FormControl
+    FormControl,
+    RadioGroup,
+    FormControlLabel,
+    Radio,
 } from "@material-ui/core";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { db } from "../../../firebase-config";
 import { addDoc, collection } from "firebase/firestore";
 
@@ -38,17 +41,90 @@ const useStyles = makeStyles((theme) => ({
             backgroundColor: "#2765B0",
         },
     },
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+    },
+    selectEmpty: {
+        marginTop: theme.spacing(2),
+    },
 }));
 
 export function Personas(props) {
     const classes = useStyles();
     const { open, handleClose, club } = props;
-    const cadeteM = ["-33 Kg", "-37 Kg", "-41 Kg", "-45 Kg", "-49 Kg", "-53 Kg", "-57 Kg", "-61 Kg", "-65 Kg", "+65 Kg"];
-    const cadeteF = ["-29 Kg", "-33 Kg", "-37 Kg", "-41 Kg", "-44 Kg", "-47 Kg", "-51 Kg", "-55 Kg", "-59 Kg", "+59 Kg"];
-    const juniorM = ["-45 Kg", "-48 Kg", "-51 Kg", "-55 Kg", "-59 Kg", "-63 Kg", "-68 Kg", "-73 Kg", "-78 Kg", "+78 Kg"];
-    const juniorF = ["-42 Kg", "-44 Kg", "-46 Kg", "-49 Kg", "-52 Kg", "-55 Kg", "-59 Kg", "-63 Kg", "-68 Kg", "+68 Kg"];
-    const seniorM = ["-54 Kg", "-58 Kg", "-63 Kg", "-68 Kg", "-74 Kg", "-80 Kg", "-87 Kg", "+87 Kg"];
-    const seniorF = ["-46 Kg", "-49 Kg", "-53 Kg", "-57 Kg", "-62 Kg", "-67 Kg", "-73 Kg", "+73 Kg"];
+    const cadeteM = [
+        "-33 Kg",
+        "-37 Kg",
+        "-41 Kg",
+        "-45 Kg",
+        "-49 Kg",
+        "-53 Kg",
+        "-57 Kg",
+        "-61 Kg",
+        "-65 Kg",
+        "+65 Kg",
+    ];
+    const cadeteF = [
+        "-29 Kg",
+        "-33 Kg",
+        "-37 Kg",
+        "-41 Kg",
+        "-44 Kg",
+        "-47 Kg",
+        "-51 Kg",
+        "-55 Kg",
+        "-59 Kg",
+        "+59 Kg",
+    ];
+    const juniorM = [
+        "-45 Kg",
+        "-48 Kg",
+        "-51 Kg",
+        "-55 Kg",
+        "-59 Kg",
+        "-63 Kg",
+        "-68 Kg",
+        "-73 Kg",
+        "-78 Kg",
+        "+78 Kg",
+    ];
+    const juniorF = [
+        "-42 Kg",
+        "-44 Kg",
+        "-46 Kg",
+        "-49 Kg",
+        "-52 Kg",
+        "-55 Kg",
+        "-59 Kg",
+        "-63 Kg",
+        "-68 Kg",
+        "+68 Kg",
+    ];
+    const seniorM = [
+        "-54 Kg",
+        "-58 Kg",
+        "-63 Kg",
+        "-68 Kg",
+        "-74 Kg",
+        "-80 Kg",
+        "-87 Kg",
+        "+87 Kg",
+    ];
+    const seniorF = [
+        "-46 Kg",
+        "-49 Kg",
+        "-53 Kg",
+        "-57 Kg",
+        "-62 Kg",
+        "-67 Kg",
+        "-73 Kg",
+        "+73 Kg",
+    ];
+    const [categoriaSeleccionada, setCategoriaSeleccionada] = useState("");
+    const [sexoSeleccionado, setSexoSeleccionado] = useState("");
+    const [pesoSeleccionado, setPesoSeleccionado] = useState("");
+    const [contenidoFiltrado, setContenidoFiltrado] = useState([]);
     const [persona, setPersona] = useState({
         Nombre: "",
         Apellido: "",
@@ -57,11 +133,56 @@ export function Personas(props) {
         Categoria: "",
         Peso: "",
         Tipo: "",
+        Sexo: "",
     });
+    console.log(persona);
+    const handleChange = (event, campo) => {
+        const { value } = event.target;
+        setPersona({ ...persona, [campo]: value });
+        console.log("id: ", campo);
+        console.log("value: ", value);
+        // setPersona({ ...persona, [id]: value });
+    };
+    //? Aqui está el como hacer la eleccion de peso
+    useEffect(() => {
+        if (persona.Categoria === "Cadete") {
+            if (persona.Sexo === "Masculino") {
+                setContenidoFiltrado(cadeteM);
+            } else {
+                setContenidoFiltrado(cadeteF);
+            }
+        } else if (persona.Categoria === "Junior") {
+            if (persona.Sexo === "Masculino") {
+                setContenidoFiltrado(juniorM);
+            } else {
+                setContenidoFiltrado(juniorF);
+            }
+        } else if (persona.Categoria === "Senior") {
+            if (persona.Sexo === "Masculino") {
+                setContenidoFiltrado(seniorM);
+            } else {
+                setContenidoFiltrado(seniorF);
+            }
+        }
+        console.log(contenidoFiltrado);
+    }, [categoriaSeleccionada, sexoSeleccionado]);
 
+    const handleChangeCategoria = (event) => {
+        const categoria = event.target.value;
+        setCategoriaSeleccionada(categoria);
+    };
+    const handleChangeSexo = (event) => {
+        const sexo = event.target.value;
+        setSexoSeleccionado(sexo);
+    };
+    const handleChangePeso = (event) => {
+        const peso = event.target.value;
+        setPesoSeleccionado(peso);
+    };
     const handleSubmit = async () => {
         await addDoc(collection(db, "Personas"), {
             ...persona,
+            Peso : pesoSeleccionado,
         });
         setTimeout(1000);
         setPersona({
@@ -72,13 +193,10 @@ export function Personas(props) {
             Categoria: "",
             Peso: "",
             Tipo: "",
+            Sexo: "",
         });
         handleClose();
     };
-    const handleChange = ({ target: { id, value } }) => {
-        setPersona({ ...persona, [id]: value });
-    };
-
     return (
         <Dialog open={open} onClose={handleClose}>
             <DialogTitle>Añadir alumno o entrenador</DialogTitle>
@@ -93,7 +211,7 @@ export function Personas(props) {
                                 fullWidth
                                 id="Nombre"
                                 label="Nombre"
-                                onChange={handleChange}
+                                onChange={(e) => handleChange(e, "Nombre")}
                                 autoFocus
                             />
                         </Grid>
@@ -104,8 +222,32 @@ export function Personas(props) {
                                 fullWidth
                                 id="Apellido"
                                 label="Apellido"
-                                onChange={handleChange}
+                                onChange={(e) => handleChange(e, "Apellido")}
                             />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <RadioGroup
+                                required
+                                fullWidth
+                                row
+                                id="Sexo"
+                                // value={value}
+                                onChange={(e) => {
+                                    handleChange(e, "Sexo");
+                                    handleChangeSexo(e);
+                                }}
+                            >
+                                <FormControlLabel
+                                    value="Masculino"
+                                    control={<Radio />}
+                                    label="Masculino"
+                                />
+                                <FormControlLabel
+                                    value="Femenino"
+                                    control={<Radio />}
+                                    label="Femenino"
+                                />
+                            </RadioGroup>
                         </Grid>
                         <Grid item xs={12} sm={3}>
                             <TextField
@@ -114,45 +256,7 @@ export function Personas(props) {
                                 fullWidth
                                 id="Edad"
                                 label="Edad"
-                                onChange={handleChange}
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={3}>
-                            <FormControl
-                                variant="outlined"
-                                fullWidth
-                            >
-                                <InputLabel id="demo-simple-select-outlined-label">
-                                    Categoria
-                                </InputLabel>
-                                <Select
-                                    id="Categoria"
-                                    onChange={handleChange}
-                                    required
-                                >
-                                    <MenuItem value={"Cadete"}>Cadete</MenuItem>
-                                    <MenuItem value={"Junior"}>Junior</MenuItem>
-                                    <MenuItem value={"Senior"}>Senior</MenuItem>
-                                    <MenuItem value={"Entrenador"}>Entrenador</MenuItem>
-                                </Select>
-                            </FormControl>
-                            {/* <TextField
-                                variant="outlined"
-                                fullWidth
-                                id="Categoria"
-                                placeholder="Cadete / Junior / Senior"
-                                label="Categoria"
-                                onChange={handleChange}
-                            /> */}
-                        </Grid>
-                        <Grid item xs={12} sm={3}>
-                            <TextField
-                                variant="outlined"
-                                required
-                                fullWidth
-                                id="Peso"
-                                label="Peso"
-                                onChange={handleChange}
+                                onChange={(e) => handleChange(e, "Edad")}
                             />
                         </Grid>
                         <Grid item xs={3}>
@@ -165,6 +269,54 @@ export function Personas(props) {
                                 placeholder="Entrenador / Competidor"
                                 onChange={handleChange}
                             />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <FormControl
+                                variant="outlined"
+                                fullWidth
+                                label="Categoria"
+                                id="Categoria"
+                            >
+                                <InputLabel>Categoria</InputLabel>
+                                <Select
+                                    id="Categoria"
+                                    onChange={(e) => {
+                                        handleChange(e, "Categoria");
+                                        handleChangeCategoria(e);
+                                    }}
+                                    required
+                                    label="Categoria"
+                                >
+                                    <MenuItem value={"Cadete"}>Cadete</MenuItem>
+                                    <MenuItem value={"Junior"}>Junior</MenuItem>
+                                    <MenuItem value={"Senior"}>Senior</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <FormControl
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="Peso"
+                                label="Peso"
+                            >
+                                <InputLabel>Peso</InputLabel>
+                                <Select
+                                    value={pesoSeleccionado}
+                                    label="Continido Filtrado"
+                                    onChange={(e) => {
+                                        handleChangePeso(e);
+                                        handleChange(e, "Peso");
+                                    }}
+                                >
+                                    {contenidoFiltrado.map((op, index) => (
+                                        <MenuItem key={index} value={op}>
+                                            {op}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                            </FormControl>
                         </Grid>
                     </Grid>
                     <Button
